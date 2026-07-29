b5e770ccc3a72b3689e1147ba26d591990267263
ombiswas <ombiswas345@gmail.com>
2026-07-29 07:07:00 +0530

    security: untrack frontend/dist build artifacts and synchronize gitignore files

--- frontend/dist/assets/ProfilePage-Bfny7QSX.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./query-DkgAO5n3.js";function r(){return e.jsx("div",{className:"p-6",children:e.jsx("h1",{className:"font-display text-2xl",children:"My Profile"})})}export{r as default};
